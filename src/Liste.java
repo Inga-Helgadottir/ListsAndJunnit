fae64e27a627f0c03f8d8ed2bcf315067b599ad1
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-07 10:33:52 +0200

    insert from head, with test

--- src/Liste.java
+++ src/Liste.java
@@ -1,6 +1,18 @@
-import javax.xml.soap.Node;
-
 public class Liste {
     Node head;
     Node tail;
+
+    public Node insertFromHead(Node n){
+        if(head == null){
+            head = n;
+            tail = n;
+            return head;
+        }
+
+        head.prev = n;
+        n.next = head;
+        head = n;
+
+        return n;
+    }
 }
---
+++ src/ListeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListeTest {
+    Liste liste = new Liste();
+
+    @Test
+    void insertFromHead() {
+
+        Node n = new Node("data");
+        Node n2 = new Node("data2");
+
+        assertEquals("data", liste.insertFromHead(n).data);
+        assertEquals("data2", liste.insertFromHead(n2).data);
+
+    }
+}
